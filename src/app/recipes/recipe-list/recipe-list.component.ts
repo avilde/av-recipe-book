0b712d2f8a42fad6093e9b56254e96a0b4a916bc
Andris Vilde <vilde.andris@gmail.com>
2020-04-12 21:31:59 +0300

    recipe list and detail layout

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.pug',
-  styleUrls: ['./recipe-list.component.styl']
+  styleUrls: ['./recipe-list.component.styl'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [new Recipe('Test', 'Test', 'Test')];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
