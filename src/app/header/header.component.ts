b9e33e575717c8ad6e857790380e3564a939ee6c
Andris Vilde <vilde.andris@gmail.com>
2020-04-12 13:12:03 +0300

    global style config

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-header',
-    templateUrl: './header.component.pug'
+    templateUrl: './header.component.pug',
+    styleUrls: ['./header.component.styl']
 })
 export class HeaderComponent {
 
