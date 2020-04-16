3aeb035fba46f47db717bdc0331030366c73257e
Vilde, Andris <andris.vilde@if.lv>
2020-04-16 07:59:11 +0300

    corrected pug syntax

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.styl']
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('amountInput') amountInputRef: ElementRef;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onAddItem() {
+
+  }
 }
