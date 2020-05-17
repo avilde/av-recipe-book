07db32739be45b366316a061f799cee7e6bb9aaa
Andris Vilde <vilde.andris@gmail.com>
2020-05-17 21:32:10 +0300

    recipe edit component files

---
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-edit',
+  templateUrl: './recipe-edit.component.pug',
+  styleUrls: ['./recipe-edit.component.styl']
+})
+export class RecipeEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
