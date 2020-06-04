d53eaf5336d92edbddb17193d05ebe04b74ae321
avilde <vilde.andris@gmail.com>
2020-06-04 12:22:12 +0300

    auth component

---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.pug',
+  styleUrls: ['./auth.component.styl'],
+})
+export class AuthComponent {}
