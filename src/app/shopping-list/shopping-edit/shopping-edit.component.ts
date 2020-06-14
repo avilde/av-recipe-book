d9c19646164ae69302cf669624b76a068e2f99b4
Andris Vilde <vilde.andris@gmail.com>
2020-06-14 18:23:44 +0300

    To shopping list fixes

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -27,7 +27,7 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
         this.shoppingListForm.setValue({
           name: this.editItem.name,
           amount: this.editItem.amount,
-          unit: this.editItem.unit
+          unit: this.editItem.unit || null
         });
       }
     );
