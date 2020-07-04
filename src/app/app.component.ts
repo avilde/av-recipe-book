41465cbd2bbcfa4abf5ceec7bb3c4d319e2c1ade
Andris Vilde <vilde.andris@gmail.com>
2020-07-04 13:50:51 +0300

    Test user footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './auth/auth.service';
 import { mySignature } from 'src/utils/commonUtils';
+import { TEST_USER } from './auth/constants';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.pug',
-  styleUrls: ['./app.component.styl']
+  styleUrls: ['./app.component.styl'],
 })
 export class AppComponent implements OnInit {
   title = 'av-recipe-book';
+  isTestUser = this.authService.isTestUser;
 
   constructor(private authService: AuthService) {}
 
   ngOnInit() {
     this.authService.autoLogin();
+    this.authService.user.subscribe((user) => {
+      console.log(user);
+      if (user) {
+        this.isTestUser = user.email === TEST_USER;
+      } else {
+        this.isTestUser = false;
+      }
+    });
     mySignature();
   }
 }
