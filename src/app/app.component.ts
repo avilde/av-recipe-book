6997978778ffc1a41c4d4bc279703af815052e20
Andris Vilde <vilde.andris@gmail.com>
2020-07-04 13:27:09 +0300

    My signature in console

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './auth/auth.service';
+import { mySignature } from 'src/utils/commonUtils';
 
 @Component({
   selector: 'app-root',
@@ -13,5 +14,6 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.authService.autoLogin();
+    mySignature();
   }
 }
